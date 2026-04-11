ce86a0e83d97bdab3d7115bed9445b591a4b6ec9
breadwars <breadwars@outlook.com>
2026-04-11 15:27:57 -0400

    added dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [roll1, setRoll1] = useState(1);
+  const [roll2, setRoll2] = useState(1);
+  const [total, setTotal] = useState(0);
+
+  function rollDice() {
+    const randomRoll1 = Math.floor(Math.random() * 6) + 1;
+    setRoll1(randomRoll1);
+    const randomRoll2 = Math.floor(Math.random() * 6) + 1;
+    setRoll2(randomRoll2);
+    setTotal((prevTotal) => prevTotal + randomRoll1 + randomRoll2);
+  }
 
   return (
-    <>
-        <p>Hello world</p>
-    </>
-  )
+    <div className="container">
+      <h1>Dice Roller</h1>
+      <div className="dice_container">
+        <div className="dice">{roll1}</div>
+        <div className="dice">{roll2}</div>
+      </div>
+      
+      <div className="last_roll">
+           <p className="text">Last Roll: {roll1}</p>
+           <p className="text">Last Roll: {roll2}</p>
+      </div>
+      <p className="text">Total Score: {total}</p>
+      <button onClick={rollDice}>Roll Dice</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
